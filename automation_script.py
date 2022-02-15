15b1a91bcfc03fac0960230d610c1f5ce5e4d354
Valmir Ganiu <valmirganiu@MBP-von-Valmir.fritz.box>
2022-02-15 20:01:36 +0100

    added support for working on macOS -> testing needed

--- automation_script.py
+++ automation_script.py
@@ -1,4 +1,4 @@
-import subprocess, webbrowser, os
+import subprocess, webbrowser, os, psutil
 
 # Writing routine for macOS
 def writing_macOS():
@@ -15,7 +15,7 @@ def writing_Windows():
     os.system('open', 'C:/Programme/Outlook/Outlook.exe')   # Path only obligatory
 
 # Writing routine for Linux
-def writing_Linux():
+#def writing_Linux():
     # open iTunes
     # open Joplin
     # open email
@@ -32,55 +32,66 @@ def coding_macOS():
     subprocess.run(['open', '/System/Applications/Utilities/Terminal.app'], check = True)
 
 # Coding routine for Windows
-def coding_Windows():
+# def coding_Windows():
 
 # Coding routine for Linux
-def coding_Linux():
+# def coding_Linux():
 
 # Working routine for macOS
 def working_macOS():
     # open Outlook
+    subprocess.run(['open', '/Applications/Microsoft\Outlook.app'], check = True)
     # open Safari with Google, Matlab
     webbrowser.open('https://google.de')
     webbrowser.open('https://matlab.com')
     # open Obsidian
     subprocess.run(['open', '/Applications/Obsidian.app'], check = True)
     # open Matlab
-    # open Finder where the Matlab-Script is at
-    # execute a git pull command to get the latest code from GitHub
+    subprocess.run(['open', '/Applications/MATLAB_R2021b.app'], check = True)
+    # open Finder where the Matlab-Script is at and get the latest version from GitLab
+    os.system('cd Documents/MATLAB/LVN_DG')
+    os.system('git pull origin/master')
+    # Check if Matlab has been closed
+    matlab_closed = "MATLAB_R2021b.app" in (i.name() for i in psutil.process_iter())
+    if matlab_closed == true:
+        os.system('git add -A')
+        commit_msg = input('please enter a commit message: ')
+        os.system('git commit -m "' + commit_msg + '"')
+        os.system('git push')
 
 # Working routine for Windows
-def working_Windows():
+# def working_Windows():
 
 # Working routine for Linux
-def working_Linux():
+# def working_Linux():
 
-work_type = input('Do you want to "write", "code", or "work"?: ')
-os = input('Are you working on "Linux", "macOS", or "Windows"?: ')
+if __name__ == '__main__':
+    work_type = input('Do you want to "write", "code", or "work"?: ')
+    os = input('Are you working on "Linux", "macOS", or "Windows"?: ')
 
-if (work_type == 'write' and os == 'macOS'):
-    writing_macOS()
+    if (work_type == 'write' and os == 'macOS'):
+        writing_macOS()
 
-elif (work_type == 'write' and os == 'Windows'):
-    writing_Windows()
+    elif (work_type == 'write' and os == 'Windows'):
+        writing_Windows()
 
-elif (work_type == 'write' and os == 'Linux'):
-    writing_Linux()
+    elif (work_type == 'write' and os == 'Linux'):
+        writing_Linux()
 
-elif (work_type == 'code' and os == 'macOS'):
-    coding_macOS()
+    elif (work_type == 'code' and os == 'macOS'):
+        coding_macOS()
 
-elif (work_type == 'code' and os == 'Windows'):
-    coding_Windows()
+    elif (work_type == 'code' and os == 'Windows'):
+        coding_Windows()
 
-elif (work_type == 'code' and os == 'Linux'):
-    coding_Linux()
+    elif (work_type == 'code' and os == 'Linux'):
+        coding_Linux()
 
-elif (work_type == 'work' and os == 'macOS'):
-    working_macOS()
+    elif (work_type == 'work' and os == 'macOS'):
+        working_macOS()
 
-elif (work_type == 'work' and os == 'Windows'):
-    working_Windows()
+    elif (work_type == 'work' and os == 'Windows'):
+        working_Windows()
 
-elif (work_type == 'work' and os == 'Linux'):
-    working_Linux()
+    elif (work_type == 'work' and os == 'Linux'):
+        working_Linux()
